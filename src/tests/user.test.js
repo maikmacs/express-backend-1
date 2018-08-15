78a2e5be48dd2de41fe4dfc6a024c2cd29b11ddf
Miguel <maik.macs@hotmail.com>
2018-08-15 08:39:09 -0500

    Create Unit Test for USER

--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -6,24 +6,55 @@ import User from '../models/users';
 import createToken from '../resolvers/createToken';
 
 describe('Prueba de User', async () => {
-  beforeEach(async () => await setUpTest());
-
-  it('Probando query sin token deberia ser null', async () => {
+  beforeAll(async () => await setUpTest());
+  it('Probando query de me sin token deberia ser null', async () => {
+    //language=GraphQL
     const query = `
-        query{
-            me{
-                _id
-                username
-                nombre
-                correo
-            }
-        }
+      query {
+          me{
+              _id
+               nombre
+              username
+          }
+      }
       `;
-
     const result = await graphql(schema, query, {}, {});
-    console.log('RESULT', result);
     const { data } = result;
     expect(data.me).toBe(null);
+  });
+
+  it('Probando query de me que deberia ser un usuario', async () => {
+    //console.log('Iniciando el test 2');
+    //language=GraphQL
+    const query = `
+    
+      query{
+          me{
+              _id,
+              nombre
+          }
+      }
+  
+  `;
+    //console.log('Paso const query');
+
+    const user = new User({
+      nombre: 'user',
+      apellidos: 'user3',
+      correo: 'test@test.com',
+      fecha_nacimiento: '1990-06-19',
+      password: 'test123',
+      username: 'user4',
+      genero: 'Masculino',
+      pais: 'Mexico',
+      ubicacion: 'casa',
+      rol: 'USER'
+    });
+    const newUser = await user.save();
+    const context = { user: newUser };
+    const result = await graphql(schema, query, {}, context);
+    const { data } = result;
+    expect(data.me._id).toBe(newUser._id.toString());
   });
 });
 
